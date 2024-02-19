49197447e845d9928f94d63b60da259c22513fbe
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-19 09:13:23 -0600

    added get and post routes to server.js

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -26,3 +26,21 @@ app.get('/api/notes', (req, res) => {
         res.json(notes);
     });
 });
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        newNote.id = notes.length > 0 ? notes[notes.length - 1].id + 1 : 1;
+        notes.push(newNote);
+        fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2), err => {
+            if (err) throw  err;
+            res.json(newNote);
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
