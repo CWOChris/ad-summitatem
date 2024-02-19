55e34da509b571f45cbda8e73433c4510025bc76
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-19 09:28:21 -0600

    added delete route

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './', 'notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -37,6 +37,19 @@ app.post('/api/notes', (req, res) => {
         fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2), err => {
             if (err) throw  err;
             res.json(newNote);
+        });
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = parseInt(req.params.id);
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== noteId);
+        fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2), err => {
+            if (err) throw err;
+            res.json({ message: 'Note deleted' });
         });
     });
 });
