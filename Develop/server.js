a4f15b9806867b718c5d1bbcae47d0979a1e6837
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-19 16:34:24 -0600

    updated server.js file to correspond with routes in routes dir

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fs = require('fs');
 const noteRoutes = require('./routes/noteRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,16 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
 app.use('/api/notes', noteRoutes);
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
+  console.log(`App listening on PORT: ${PORT}`);
 });
