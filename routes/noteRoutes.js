5b0769181757ea3047b0b217d1b9d891c4d35d1c
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-19 17:18:31 -0600

    change routes to accomodate file structure change for github pages

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -4,20 +4,20 @@ const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    fs.readFile('./db.json', 'utf8', (err, data) => {
+    fs.readFile('/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     });
 });
 
 router.post('/', (req, res) => {
-    fs.readFile('./db.json', 'utf8', (err, data) => {
+    fs.readFile('/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
         const newNote = req.body;
         newNote.id = uuidv4();
         notes.push(newNote);
-        fs.writeFile('./db.json', JSON.stringify(notes), 'utf8', (err) => {
+        fs.writeFile('/db.json', JSON.stringify(notes), 'utf8', (err) => {
             if (err) throw err;
             res.json(newNote);
         });
@@ -26,11 +26,11 @@ router.post('/', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     const noteId = req.params.id;
-    fs.readFile('./db.json', 'utf8', (err, data) => {
+    fs.readFile('/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         let notes = JSON.parse(data);
         notes = notes.filter(note => note.id !== noteId);
-        fs.writeFile('./db.json', JSON.stringify(notes), 'utf8', (err) => {
+        fs.writeFile('/db.json', JSON.stringify(notes), 'utf8', (err) => {
             if (err) throw err;
             res.send('Note deleted');
         });
