0be7d7f970a2564993d74d47f2784e6c12f3d6ab
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-19 09:03:54 -0600

    added GET routes to server.js

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -10,3 +10,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf-8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
