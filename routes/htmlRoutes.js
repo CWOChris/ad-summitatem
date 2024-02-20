a7c834f5bba1e9ce28e828cc78b8b76b3804607f
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-19 18:57:27 -0600

    reverting...

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/ad-summitatem/public/notes.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));;
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));;
 });
 
 router.get('*', (req, res) => {
