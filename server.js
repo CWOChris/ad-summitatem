b542b050dfd2bf602c29a29885edf325add0d356
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-20 11:51:37 -0600

    moved db.json into its own directory to make change to server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.static('db'));
 
 app.use('/ad-summitatem/api/notes', noteRoutes);
 app.use('/', htmlRoutes);
